e0cdaf7e3d3130da093aded8d3a4d784d0713259
Todorovic Sara <Sara.Todorovic@env.fi>
2020-10-21 23:37:26 +0300

    Update app.R

--- app.R
+++ app.R
@@ -16,7 +16,7 @@ wd <- getwd()
 #csspath <- file.path(wd, "app_style.css")
 
 # App version
-app_v <- "0001 (19.10.2020)"
+app_v <- "0002 (21.10.2020)"
 
 # Install libraries
 #install.packages("shiny")
@@ -39,30 +39,30 @@ setwd("C:/Users/e1007642/Documents/ClimVeturi")
 # plus reference dataframes for all areas (162 + 27)
 
 nyky <- read.table("data/suomiq__control_run__nyky_nyky_climveturi_0320_2.dat", sep = "", dec =".", header = FALSE,
-                        colClasses = c(rep("character", 115), rep("NULL", 745)))
+                   colClasses = c(rep("character", 115), rep("NULL", 745)))
 
 nyky <- as.data.frame(sapply(nyky, as.numeric))
 
 colnames(nyky) = c("TimeStamp","1", "2","3","4.9","4.8","4.7","4.6","4.5","4.4","4.3","4.2","4.1","5","6","7","8","9","10","11","12","13",
-                        "14.9","14.8","14.7","14.6","14.5","14.4","14.3","14.2","14.1","15","16","17","18","19","20","21","22","23","24","25",
-                        "26","27","28","29","30","31","32","33","34","35.9","35.8","35.7","35.6","35.5","35.4","35.3","35.2","35.1","36","37",
-                        "38","39","40","41","42","43","44","45","46","47","48","49","50","51","52","53","54","55","56","57","58","59.9","59.8",
-                        "59.7","59.6","59.5","59.4","59.3","59.2","59.1","60","61","62","63","64","65.9","65.8","65.7","65.6","65.5","65.4",
-                        "65.3","65.2","65.1","66","67","68","69","70","71","72","73","74")
+                   "14.9","14.8","14.7","14.6","14.5","14.4","14.3","14.2","14.1","15","16","17","18","19","20","21","22","23","24","25",
+                   "26","27","28","29","30","31","32","33","34","35.9","35.8","35.7","35.6","35.5","35.4","35.3","35.2","35.1","36","37",
+                   "38","39","40","41","42","43","44","45","46","47","48","49","50","51","52","53","54","55","56","57","58","59.9","59.8",
+                   "59.7","59.6","59.5","59.4","59.3","59.2","59.1","60","61","62","63","64","65.9","65.8","65.7","65.6","65.5","65.4",
+                   "65.3","65.2","65.1","66","67","68","69","70","71","72","73","74")
 
 
-# 2. Valitaan tarkasteltavat sarakkeet (Nooran viesti 21.9 + 19.10) --> MUOKATTAVISSA, lisÃ¤Ã¤ vain vesistÃ¶n numeron
+# 2. Valitaan tarkasteltavat sarakkeet (Nooran viesti 21.9 + 19.10) --> MUOKATTAVISSA, lisää vain vesistön numeron
 
 nyky <- nyky[c("TimeStamp", "4.1", "14.1", "14.6", "14.7", "21", "28", "35.1", "35.7", "35.9", "37", "41", "42", "44", "49",
-                    "53", "54", "57", "59.1", "59.9","61", "64","65.1","65.6", "65.8","67","68", "71")]
+               "53", "54", "57", "59.1", "59.9","61", "64","65.1","65.6", "65.8","67","68", "71")]
 
-# NimetÃ¤Ã¤n vesistÃ¶alueet tulevien ongelmien vÃ¤lttÃ¤miseksi (jos muutettu edellistÃ¤, lisÃ¤tÃ¤Ã¤n myÃ¶s nimi tÃ¤hÃ¤n litaniaan oikealle kohdalle)
-colnames(nyky) <- c("TimeStamp", "Vuoksi", "Kymijoki", "SaarijÃ¤rven reitti", "Rautalammin reitti","Vantaanjoki",
-                    "Aurajoki","KokemÃ¤enjoki","LÃ¤ngelmÃ¤veden-Hauhon reitti", 
-                    "Loimijoki","LapvÃ¤Ã¤rtinjoki", "Laihianjoki",
-                    "KyrÃ¶njoki", "Lapuanjoki","Perhonjoki", 
-                    "Kalajoki", "PyhÃ¤joki", "Siikajoki","Ala-Oulujoki", "OntojÃ¤rvi-Lentua","Iijoki", "Simojoki",
-                    "Ala-Kemijoki","YlÃ¤-Ounasjoki", "Kitinen", "Tornionjoki-Muonionjoki","Teno", "Paatsjoki")
+# Nimetään vesistöalueet tulevien ongelmien välttämiseksi (jos muutettu edellistä, lisätään myös nimi tähän litaniaan oikealle kohdalle)
+colnames(nyky) <- c("TimeStamp", "Vuoksi", "Kymijoki", "Saarijärven reitti", "Rautalammin reitti","Vantaanjoki",
+                    "Aurajoki","Kokemäenjoki","Längelmäveden-Hauhon reitti", 
+                    "Loimijoki","Lapväärtinjoki", "Laihianjoki",
+                    "Kyrönjoki", "Lapuanjoki","Perhonjoki", 
+                    "Kalajoki", "Pyhäjoki", "Siikajoki","Ala-Oulujoki", "Ontojärvi-Lentua","Iijoki", "Simojoki",
+                    "Ala-Kemijoki","Ylä-Ounasjoki", "Kitinen", "Tornionjoki-Muonionjoki","Teno", "Paatsjoki")
 
 nyky$TimeStamp <- ymd(nyky$TimeStamp)
 nyky <- nyky[!(format(nyky$TimeStamp, "%m") == "02" & format(nyky$TimeStamp, "%d")==29), , drop = FALSE]
@@ -122,12 +122,12 @@ for (e in list39) {
   a <- a[c("TimeStamp", "4.1", "14.1", "14.6", "14.7", "21", "28", "35.1", "35.7", "35.9", "37", "41", "42", "44", "49",
            "53", "54", "57", "59.1", "59.9","61", "64","65.1","65.6", "65.8","67","68", "71")]
   
-  colnames(a) <- c("TimeStamp", "Vuoksi", "Kymijoki", "SaarijÃ¤rven reitti", "Rautalammin reitti","Vantaanjoki",
-                   "Aurajoki","KokemÃ¤enjoki","LÃ¤ngelmÃ¤veden-Hauhon reitti", 
-                   "Loimijoki","LapvÃ¤Ã¤rtinjoki", "Laihianjoki",
-                   "KyrÃ¶njoki", "Lapuanjoki","Perhonjoki", 
-                   "Kalajoki", "PyhÃ¤joki", "Siikajoki","Ala-Oulujoki", "OntojÃ¤rvi-Lentua","Iijoki", "Simojoki",
-                   "Ala-Kemijoki","YlÃ¤-Ounasjoki", "Kitinen", "Tornionjoki-Muonionjoki","Teno", "Paatsjoki")
+  colnames(a) <- c("TimeStamp", "Vuoksi", "Kymijoki", "Saarijärven reitti", "Rautalammin reitti","Vantaanjoki",
+                   "Aurajoki","Kokemäenjoki","Längelmäveden-Hauhon reitti", 
+                   "Loimijoki","Lapväärtinjoki", "Laihianjoki",
+                   "Kyrönjoki", "Lapuanjoki","Perhonjoki", 
+                   "Kalajoki", "Pyhäjoki", "Siikajoki","Ala-Oulujoki", "Ontojärvi-Lentua","Iijoki", "Simojoki",
+                   "Ala-Kemijoki","Ylä-Ounasjoki", "Kitinen", "Tornionjoki-Muonionjoki","Teno", "Paatsjoki")
   
   a$TimeStamp <- ymd(a$TimeStamp)
   a <- a[!(format(a$TimeStamp, "%m") == "02" & format(a$TimeStamp, "%d")==29), , drop = FALSE]
@@ -173,13 +173,13 @@ for (e in list69) {
   a <- a[c("TimeStamp", "4.1", "14.1", "14.6", "14.7", "21", "28", "35.1", "35.7", "35.9", "37", "41", "42", "44", "49",
            "53", "54", "57", "59.1", "59.9","61", "64","65.1","65.6", "65.8","67","68", "71")]
   
-  # NimetÃ¤Ã¤n vesistÃ¶alueet vÃ¤lttÃ¤miseksi (jos muutettu edellistÃ¤, lisÃ¤tÃ¤Ã¤n myÃ¶s nimi tÃ¤hÃ¤n litaniaan oikealle kohdalle)
-  colnames(a) <- c("TimeStamp", "Vuoksi", "Kymijoki", "SaarijÃ¤rven reitti", "Rautalammin reitti","Vantaanjoki",
-                   "Aurajoki","KokemÃ¤enjoki","LÃ¤ngelmÃ¤veden-Hauhon reitti", 
-                   "Loimijoki","LapvÃ¤Ã¤rtinjoki", "Laihianjoki",
-                   "KyrÃ¶njoki", "Lapuanjoki","Perhonjoki", 
-                   "Kalajoki", "PyhÃ¤joki", "Siikajoki","Ala-Oulujoki", "OntojÃ¤rvi-Lentua","Iijoki", "Simojoki",
-                   "Ala-Kemijoki","YlÃ¤-Ounasjoki", "Kitinen", "Tornionjoki-Muonionjoki","Teno", "Paatsjoki")
+  # Nimetään vesistöalueet välttämiseksi (jos muutettu edellistä, lisätään myös nimi tähän litaniaan oikealle kohdalle)
+  colnames(a) <- c("TimeStamp", "Vuoksi", "Kymijoki", "Saarijärven reitti", "Rautalammin reitti","Vantaanjoki",
+                   "Aurajoki","Kokemäenjoki","Längelmäveden-Hauhon reitti", 
+                   "Loimijoki","Lapväärtinjoki", "Laihianjoki",
+                   "Kyrönjoki", "Lapuanjoki","Perhonjoki", 
+                   "Kalajoki", "Pyhäjoki", "Siikajoki","Ala-Oulujoki", "Ontojärvi-Lentua","Iijoki", "Simojoki",
+                   "Ala-Kemijoki","Ylä-Ounasjoki", "Kitinen", "Tornionjoki-Muonionjoki","Teno", "Paatsjoki")
   
   
   a$TimeStamp <- ymd(a$TimeStamp)
@@ -302,20 +302,20 @@ for (nimi in colnames(list69[["ka_69"]])) {
 
 
 # Parameters
-locations <- c("Vuoksi", "Kymijoki", "SaarijÃ¤rven reitti", "Rautalammin reitti",
-               "Vantaanjoki", "Aurajoki","KokemÃ¤enjoki","LÃ¤ngelmÃ¤veden-Hauhon reitti", 
-                "Loimijoki","LapvÃ¤Ã¤rtinjoki", "Laihianjoki",
-                "KyrÃ¶njoki", "Lapuanjoki","Perhonjoki", 
-                "Kalajoki", "PyhÃ¤joki", "Siikajoki","Ala-Oulujoki", "OntojÃ¤rvi-Lentua","Iijoki", "Simojoki",
-                "Ala-Kemijoki","YlÃ¤-Ounasjoki", "Kitinen", "Tornionjoki-Muonionjoki","Teno", "Paatsjoki")
+locations <- c("Vuoksi", "Kymijoki", "Saarijärven reitti", "Rautalammin reitti",
+               "Vantaanjoki", "Aurajoki","Kokemäenjoki","Längelmäveden-Hauhon reitti", 
+               "Loimijoki","Lapväärtinjoki", "Laihianjoki",
+               "Kyrönjoki", "Lapuanjoki","Perhonjoki", 
+               "Kalajoki", "Pyhäjoki", "Siikajoki","Ala-Oulujoki", "Ontojärvi-Lentua","Iijoki", "Simojoki",
+               "Ala-Kemijoki","Ylä-Ounasjoki", "Kitinen", "Tornionjoki-Muonionjoki","Teno", "Paatsjoki")
 
 timeframe_names <- c("2010-2039", "2040-2069") # 1, 2
-scenario_names <- c("LÃ¤mmin ja mÃ¤rkÃ¤", "KylmÃ¤", "Usean skenaarion keskiarvo") # 1, 2, 3
+scenario_names <- c("Lämmin ja märkä", "Kylmä", "Usean skenaarion keskiarvo") # 1, 2, 3
 
 
 
 #varit <- c("ref_ka" = "gray21", "ref_maxmin" = "grey75", "lammin_ka" = "tan1", "lammin_maxmin" ="tan1", 
-           #"kylma_ka" = "turquoise3", "kylma_maxmin" = "turquoise3", "ka45_ka" = "indianred2", "ka45_maxmin" = "indianred2")
+#"kylma_ka" = "turquoise3", "kylma_maxmin" = "turquoise3", "ka45_ka" = "indianred2", "ka45_maxmin" = "indianred2")
 #load("ref_list.RData")
 
 #### ShinyApp Server -----------------------------------------------------------
@@ -323,10 +323,10 @@ scenario_names <- c("LÃ¤mmin ja mÃ¤rkÃ¤", "KylmÃ¤", "Usean skenaarion keskiarvo"
 server <- function(input, output){
   
   #output$tab <- renderTable({
-    
-    #thisName <- paste(input$location, input$timeframe, input$scenario, sep = "_")
-    #message(thisName)
-    #list_dfs[[thisName]]
+  
+  #thisName <- paste(input$location, input$timeframe, input$scenario, sep = "_")
+  #message(thisName)
+  #list_dfs[[thisName]]
   #})
   
   output$plo <- renderPlot({
@@ -334,12 +334,22 @@ server <- function(input, output){
     thisName <- paste(input$location, input$timeframe, input$scenario, sep = "_")
     thisPlot <- scen_list[[thisName]]
     
-    plo <- ggplot(data = thisPlot, 
-                  aes(x = D_M, y = mean, group = "group")) +
-      geom_line(size = 1.2) +
-      geom_ribbon(aes(ymin=min, ymax=max, colour = "red"),
-                  fill=NA, size = 1.1, linetype = 2) +
-       
+    nameRef <- paste(input$location, "ref", sep ="_")
+    thisRefPlot <- ref_list[[nameRef]]
+    
+    plo <- ggplot(data = thisRefPlot, 
+                  aes(x = D_M, y = mean)) +
+      geom_line(aes(y = mean, colour = "gray21"), size = 1.2) +
+      labs(title= nimi, 
+           y = expression(paste("Virtaama (", m^3,"/s)", sep="")))+
+      geom_ribbon(aes(ymin=min, ymax=max, colour = NA),
+                  fill="grey75", alpha = 0.5) +
+      
+      geom_line(data=thisPlot, aes(y = mean, colour = "blue"), 
+                size = 1.2) +
+      geom_ribbon(data=thisPlot, aes(ymin = min, ymax = max, colour = "blue"),
+                  fill = NA, size = 1.1, linetype = 3) +
+      
       
       # MUUT ASETUKSET
       # Y ja X akselit
@@ -354,15 +364,15 @@ server <- function(input, output){
             panel.background = element_blank(),
             panel.grid.major = element_blank(),
             axis.line = element_line(colour="grey"),
-            legend.spacing.y = unit(-0.4, "cm"),
-            legend.position = c(1, 1.05),
-            legend.justification = c("right", "top"),
-            legend.box.just = "left",
-            legend.margin = margin(6, 6, 6, 6),
-            legend.background = element_blank(),
-            legend.text = element_text(size=10),
-            legend.key.size = unit(0.5, "cm"),
-            legend.box.background = element_rect(alpha("white", 0.3), color =NA),
+            #legend.spacing.y = unit(-0.4, "cm"),
+            #legend.position = c(1, 1.05),
+            #legend.justification = c("right", "top"),
+            #legend.box.just = "left",
+            #legend.margin = margin(6, 6, 6, 6),
+            #legend.background = element_blank(),
+            #legend.text = element_text(size=10),
+            #legend.key.size = unit(0.5, "cm"),
+            #legend.box.background = element_rect(alpha("white", 0.3), color =NA),
             plot.title = element_text(size=12, face="bold")) 
     
     plo
@@ -379,7 +389,7 @@ ui <- shinyUI(fluidPage(
   titlePanel(h1("TESTI"), 
              windowTitle = "ClimVeturi visualisoinnit"),
   
-
+  
   ### sidebarLayout contents ---------------------------------------------------
   sidebarLayout(
     sidebarPanel(
@@ -388,7 +398,7 @@ ui <- shinyUI(fluidPage(
       
       selectInput(
         inputId = "location", 
-        label = HTML("Valitse vesistÃ¶alue"),
+        label = HTML("Valitse vesistöalue"),
         choices = locations),
       
       radioButtons(
